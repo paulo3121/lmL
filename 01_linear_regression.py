dce4737872aec2da4dd42c1a2bb59519e405cdab
paulo3121 <bisdusty@gmail.com>
2025-04-26 11:15:25 -0300

    .

--- 01_linear_regression.py
+++ 01_linear_regression.py
@@ -48,14 +48,11 @@ EPS = 5e-5
 RATE = 75e-3
 
 for epoch in range(50):
-    erro = cost(w, s)
     dcost_w = (cost(w + EPS, s) - cost(w, s)) / EPS
     dcost_s = (cost(w, s + EPS) - cost(w, s)) / EPS
     w -= RATE * dcost_w
     s -= 3 * RATE * dcost_s
     print(f"epoch: {epoch:2} | MSE: {cost(w, s):15.11f} | y = : {w:.2f}x + {s:.2f}")
-    if erro < cost(w, s):
-        break
 
 print("-" * 50)
 print(f"true:  y = {a:.2f}x + {b:.2f}")
